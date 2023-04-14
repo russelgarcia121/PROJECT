521d0ceb0348eb9fd49905c12a366d48728f2018
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-14 10:26:23 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.title("How Great is our God?")
-st.text("my God, my Author")
-st.markdown("date: 04/13/23")
+st.text("our God, our Author")
+st.markdown("date: 04/14/23")
 
 import streamlit as st
 
 num1 = st.number_input('Enter the 1st number')
-num2 = st.number_input('Enter the 2st number')
+num2 = st.number_input('Enter the 2nd number')
 ans = num1 + num2
 prod= num1 * num2
 # Print the name if it's not empty
