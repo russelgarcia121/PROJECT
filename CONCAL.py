8e6ab0ebfbbba9d88734a2319a92db9da3d2b1dc
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-14 10:35:38 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -13,17 +13,18 @@ cost = st.number_input("The cost per kilowatt-hour in pesos:")
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # add 1st app
 A1N = st.text_input("Name of Appliance:")
-st.write("How many", A1N, "are you using?")
-A1M = st.number_input("")
-st.write("What is the wattage of", A1N, "?")
-A1W = st.number_input("")
-A1B = st.number_input("How many days in a month do you use this?")
-while A1B > 31 or A1B < 1:
+if A1N:
+    st.write("How many", A1N, "are you using?")
+    A1M = st.number_input("")
+    st.write("What is the wattage of", A1N, "?")
+    A1W = st.number_input("")
     A1B = st.number_input("How many days in a month do you use this?")
+    while A1B > 31 or A1B < 1:
+        A1B = st.number_input("How many days in a month do you use this?")
 
-A1D = st.number_input("How many hours in a day do you use this?")
-while A1D > 24 or A1D < 0.00001:
     A1D = st.number_input("How many hours in a day do you use this?")
+    while A1D > 24 or A1D < 0.00001:
+        A1D = st.number_input("How many hours in a day do you use this?")
 
 ask1 = 2
 
