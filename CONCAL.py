6e21e03142564953a75a2855ce9fd2fb170600aa
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-14 10:59:22 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -1,8 +1,3 @@
-# A()N ay Name
-# A()M ay Quantity
-# A()W ay Wattage
-# A()B ay ilang Day in a Month ginagamit
-# A()D ay ilang Hour in a Day ginagamit
 import streamlit as st
 st.title("Welcome to CONCAL!")
 st.text("")
@@ -17,16 +12,16 @@ if A1N:
     A1M = st.number_input(f"How many {A1N} are you using?")
     A1W = st.number_input(f"What is the wattage of {A1N} ?")
     A1B = st.number_input("How many days in a month do you use this?")
-        while A1B > 31 or A1B < 1:
-            A1B = st.number_input("How many days in a month do you use this?")
+    while A1B > 31 or A1B < 1:
+        A1B = st.number_input("How many days in a month do you use this?")
+    A1D = st.number_input("How many hours in a day do you use this?")
 
+    while A1D > 24 or A1D < 0.00001:
         A1D = st.number_input("How many hours in a day do you use this?")
-        while A1D > 24 or A1D < 0.00001:
-            A1D = st.number_input("How many hours in a day do you use this?")
 
 ask1 = 2
 
-if ask1 ==2:
+if ask1 == 2:
     # INDIVIDUALLY: para ma compute ang average use (hour) kada araw over the month.
     A1F = (A1B/30) * A1D
 
@@ -52,7 +47,8 @@ if ask1 ==2:
         if app > max_app:
             max_app = app
     if max_app == A1con:
-        st.write("Ang may pinkamataas na konsumo ay ang (",A1N,") na may consumption na:",A1con)
+        st.write("Ang may pinkamataas na konsumo ay ang (",
+                 A1N, ") na may consumption na:", A1con)
 
     # OUTPUT
     st.write("(1)The consumption of", A1N, "is", A1con, "pesos")
