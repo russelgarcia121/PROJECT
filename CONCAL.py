c92fb7b81e5c62efa7e439230f2fe5b26402bb5d
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-15 19:17:42 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -23,16 +23,7 @@ if A1N:
 if ask1 == 1:  # add app2 (1st line)
     st.markdown("## **2nd APPLIANCE!**")
     A2N = st.text_input("Name of 2nd appliance: ")
-    ask1 = st.number_input(
-        "Add 2nd appliance (enter 1), No more appliances (enter 2): ", value=0, step=1)
-
-    while ask1 > 2 or ask1 < 1:  # ask for input until ask1 is 1 or 2
-        ask1 = st.number_input(
-            "Add 2nd appliance (enter 1), No more appliances (enter 2): ", value=0, step=1)
-        
-    if A2N:
-        A2M = st.number_input(f"How many {A2N} are you using?",value=0, step=1)
-        ask2 = 2
+    ask2 = 2
 
     if ask2 == 2:
 
