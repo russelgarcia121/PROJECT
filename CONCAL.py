08c5879c8136ffeea6c542aab6628a7ead222855
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-15 19:23:05 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -15,29 +15,29 @@ A1N = st.text_input("Name of 1st appliance:")
 if A1N:
     A1M = st.number_input(f"How many {A1N} are you using?", value=0, step=1)
     ask1 = st.number_input(
-        "Add 2nd appliance (enter 1), No more appliances (enter 2): ", value=0, step=1)
+        "Add 2nd appliance (enter 1), No more appliances (enter 2): ", value=0, step=1, key="ask1")
 
     while ask1 > 2 or ask1 < 1:  # ask for input until ask1 is 1 or 2
         ask1 = st.number_input(
-            "Add 2nd appliance (enter 1), No more appliances (enter 2): ", value=0, step=1)
-
+            "Add 2nd appliance (enter 1), No more appliances (enter 2): ", value=0, step=1, key="ask1")
+        
 if ask1 == 1:
     # add app2
     st.markdown("## **2nd APPLIANCE!**")
-    A2N = st.text_input("Name of 2nd appliance: ")
+    A2N = st.text_input("Name of 2nd appliance: ", key="A2N")
 
     if A2N:
-        A2M = st.number_input(f"How many {A2N} are you using?", value=0, step=1)
+        A2M = st.number_input(f"How many {A2N} are you using?", value=0, step=1, key="A2M")
         ask2 = st.number_input(
-            "No more appliances (enter 2): ", value=0, step=1)
+            "No more appliances (enter 2): ", value=0, step=1, key="ask2")
 
         while ask2 > 2 or ask2 < 1:
             ask2 = st.number_input(
-                "No more appliances (enter 2): ", value=0, step=1)
+                "No more appliances (enter 2): ", value=0, step=1, key="ask2")
 
-        if ask2 == 2:
-            st.write(A1N)
-            st.write(A2N)
+    if ask2 == 2:
+        st.write(A1N)
+        st.write(A2N)
 
 if ask1 == 2:
     st.write(A1N)
