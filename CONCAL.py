769b4241a4c2513e1cfe5038144ba6a4adff26a6
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-16 13:32:28 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -3,7 +3,9 @@ import streamlit as st
 # List of fonts to use
 fonts = ["Arial", "Times New Roman", "Verdana", "Georgia", "Comic Sans MS"]
 
-# Loop through the fonts and display "Hi" in each font
-for i in range(5):
-    font = fonts[i]
-    st.write(f"<span style='font-family:{font}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+# Display "Hi" in each font
+st.write(f"<span style='font-family:{fonts[0]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:{fonts[1]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:{fonts[2]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:{fonts[3]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:{fonts[4]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
