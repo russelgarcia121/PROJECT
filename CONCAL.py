369fdb02a2f1a0ce994b31d3443388087902b890
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-16 14:18:04 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -8,9 +8,10 @@ st.text("")
 st.write("<span style='font-family:Times New Roman; font-size:18px;'>&#10003; Calculates consumption and bill for each appliance.</span></li></ul>", unsafe_allow_html=True)
 st.text("")
 st.write("<span style='font-family:Times New Roman; font-size:18px;'>&#10003;Computes total bill and power consumption.</span></li></ul>", unsafe_allow_html=True)
-
-cost = st.text_input("What is your name?", value="", max_chars=None,
-                       key=None, type='default', help=None, font=("Calibri", 16))
+font = "Times New Roman"
+size = 28
+cost = st.number_input("What is your name?", value="", max_chars=None,
+                       key=None, type='default', help=None, font=(font, size))
 # cost = st.number_input("The cost per kilowatt-hour in pesos:",font=("Calibri",16))
 ask1 = 0
 ask2 = 0
