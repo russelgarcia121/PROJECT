f0ac00a6f91af001936576a536e65d33558b19fe
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-16 13:34:37 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -4,4 +4,4 @@ st.write(f"<span style='font-family:Arial; font-size:36px;'>Hi</span>", unsafe_a
 st.write(f"<span style='font-family:Times New Roman; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
 st.write(f"<span style='font-family:Verdana; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
 st.write(f"<span style='font-family:Georgia; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
-st.write(f"<span style='font-family:Comi Sans MS; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:Comic Sans MS; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
