5190b3975443158803eebb1fe8ddadebf231d584
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-16 13:34:02 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-# List of fonts to use
-fonts = ["Arial", "Times New Roman", "Verdana", "Georgia", "Comic Sans MS"]
-
-# Display "Hi" in each font
-st.write(f"<span style='font-family:{fonts[0]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
-st.write(f"<span style='font-family:{fonts[1]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
-st.write(f"<span style='font-family:{fonts[2]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
-st.write(f"<span style='font-family:{fonts[3]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
-st.write(f"<span style='font-family:{fonts[4]}; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:Arial; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:Times New Roman; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:Verdana; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:Georgia; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
+st.write(f"<span style='font-family:Comi Sans MS; font-size:36px;'>Hi</span>", unsafe_allow_html=True)
