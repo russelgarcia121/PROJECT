3b746446fd0134613624f4e34d753b541da4a464
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-17 14:39:36 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -3,15 +3,15 @@ import streamlit as st
 # Set the background color
 bgcolor = '#f5f5f5' # Light gray
 
-# Set the page config
-st.set_page_config(
-    page_title="My Streamlit App",
-    page_icon=":sunglasses:",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    background_color=bgcolor,
-)
+# # Set the page config
+# st.set_page_config(
+#     page_title="My Streamlit App",
+#     page_icon=":sunglasses:",
+#     layout="wide",
+#     initial_sidebar_state="expanded",
+#     background_color=bgcolor,
+# )
 
-# Create your Streamlit app here
-st.title("Welcome to my app!")
-st.write("This app has a custom background color.")
+# # Create your Streamlit app here
+# st.title("Welcome to my app!")
+# st.write("This app has a custom background color.")
