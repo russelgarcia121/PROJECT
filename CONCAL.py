4892d70e65019296b2979e6856e1f0b98490b6d4
russelgarcia121 <126207450+russelgarcia121@users.noreply.github.com>
2023-04-18 20:43:29 +0800

    Update CONCAL.py

--- CONCAL.py
+++ CONCAL.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+# Ask the user for their name
+name = st.text_input('Enter your name:')
+
 # Create two columns to display the hour and minute inputs side by side
 col1, col2 = st.beta_columns(2)
 
@@ -7,5 +10,5 @@ col1, col2 = st.beta_columns(2)
 hours = col1.number_input('Enter the number of hours you use the charger:', min_value=0, value=0)
 minutes = col2.number_input('Enter the number of minutes you use the charger:', min_value=0, max_value=59, value=0)
 
-# Print the number of hours and minutes
-st.write(f"You use the charger for {hours} hours and {minutes} minutes.")
+# Print the user's name and the number of hours and minutes
+st.write(f"{name} uses the charger for {hours} hours and {minutes} minutes.")
